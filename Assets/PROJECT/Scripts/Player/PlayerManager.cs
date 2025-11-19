0d1f0fd712235da2c06349e90c0e89087f2049ea
Syslik <syslikcompany@gmail.com>
2025-11-19 17:53:16 +0300

    dialogue camera fix

--- Assets/PROJECT/Scripts/Player/PlayerManager.cs
+++ Assets/PROJECT/Scripts/Player/PlayerManager.cs
@@ -1,9 +1,11 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public GameObject cinemachineCamera;
     bool canMove = true;
-    public bool CanMove { get { return canMove; } set { print("Set can move: " + value); canMove = value; } }
+    public bool CanMove { get { return canMove; } set { print("Set can move: " + value); cinemachineCamera.GetComponent<CinemachineInputAxisController>().enabled = value; canMove = value; } }
 
     public void ShowCursor(bool show)
     {
