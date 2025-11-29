82ac47953da9c4a2ad4c04dd144fff6483c48cd0
Syslik <syslikcompany@gmail.com>
2025-11-29 18:29:33 +0300

    added faces

---
+++ Assets/PROJECT/Scripts/Player/Face.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    public string textureParamName = "Texture";
+    public Material targetMat;
+    [Tooltip("обычное - улыбка - рот")]
+    public List<Texture> faces = new List<Texture>();
+    public void SetFace(FaceType face)
+    {
+        targetMat.SetTexture(textureParamName, faces[(int)face]);
+    }
+}
+public enum FaceType
+{
+    def,
+    smile,
+    mouth
+}
