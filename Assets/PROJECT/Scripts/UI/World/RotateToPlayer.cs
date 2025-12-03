319cbc83a8d4766bc179b4e9bb680a3b3c0d7a1c
Syslik <syslikcompany@gmail.com>
2025-12-03 15:00:58 +0300

    fixed popupui

--- Assets/PROJECT/Scripts/UI/World/RotateToPlayer.cs
+++ Assets/PROJECT/Scripts/UI/World/RotateToPlayer.cs
@@ -12,7 +12,11 @@ public class RotateToPlayer : MonoBehaviour
     {
         Vector3 dir = transform.position - obj.transform.position;
         Vector3 rot = Vector3.RotateTowards(transform.forward, dir, Time.deltaTime * 1000, 0f);
-
+        
         transform.rotation = Quaternion.LookRotation(rot);
+    }
+    private void FixedUpdate()
+    {
+        obj = !CameraSwitcher.Instance.renderer.enabled ? CameraSwitcher.Instance.firstPersonCamera.gameObject : CameraSwitcher.Instance.thirdPersonCamera.gameObject;
     }
 }
